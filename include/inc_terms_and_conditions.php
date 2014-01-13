2da2c727ba3455eb434eceea055902cd344d7448
Acube Innovation <pramodgmenon@acube.co>
2014-01-13 13:31:13 +0530

    first commit

---
+++ include/inc_terms_and_conditions.php
@@ -0,0 +1,8 @@
+<div class="innercontainer-blk">
+					<p class="heading">Terms &amp; Conditions</p>
+					<div class="sixteen columns mright8">
+						<div class="inner-box">
+							<p class="bottom-1 description">1) Contents to be filled.</p>							
+						</div>	
+					</div>
+				</div>
